713990760f8e17362278658dc76ee44912269581
SANJAY P <102913346+2k3-sanjay@users.noreply.github.com>
2023-10-02 12:04:28 +0530

    login
    login page

--- src/Components/LoginPage.js
+++ src/Components/LoginPage.js
@@ -1,9 +1,15 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import Signinmodel from './Model/signinmodel';
 
 function LoginPage() {
+
+
+  const[userDetails,setUserDetails]=useState([])
 useEffect(()=>{
-    localStorage.setItem("userDetails",JSON.stringify([{userName:"sanjay",emailId:"sanjay@gmail.com",password:"12345"}]));
+  setUserDetails(JSON.parse(localStorage.getItem("userDetails")))
 },[])
+console.log("sdfsdf",userDetails)
+
   return (
     <>
       <section class="vh-100">
@@ -53,39 +59,8 @@ useEffect(()=>{
     </div>
   </div>
 </section>
+<Signinmodel userDetails={userDetails}/>
 
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-      <form>
-    <div class="mb-3">
-    <label for="username" class="form-label">User Name</label>
-    <input type="input" class="form-control" placeholder='Enter your name' id="username"/>
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputEmail1" class="form-label">Email address</label>
-    <input type="email" class="form-control" id="exampleInputEmail1"  placeholder='Enter your Email' aria-describedby="emailHelp"/>
-    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
-  </div>
-  <div class="mb-3">
-    <label for="exampleInputPassword1" class="form-label" >Password</label>
-    <input type="password" class="form-control"  placeholder='Enter your password' id="exampleInputPassword1"/>
-  </div>
- 
-</form>
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
     </>
   )
 }
